438bedad967ece086a1904af2edd92866bec82b2
lmonty22 <lindsay.montgomery@olympian.org>
2020-08-04 13:53:26 -0400

    basic login with username

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+
+    def login 
+        user = User.all.find{|u| u.username == params['username']}
+        if !user 
+            user = User.create(username: params['username'])
+        end
+        render json: user
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  resources :picks
-  resources :contestants
-  resources :seasons
-  resources :league_members
-  resources :games
-  resources :leagues
-  resources :users
+  post '/login', to: 'users#login'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
