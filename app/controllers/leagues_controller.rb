de38c3d44f9974913bf84b1ea981e92866a740a9
lmonty22 <lindsay.montgomery@olympian.org>
2020-08-07 14:49:16 -0400

    create league

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -1,2 +1,13 @@
 class LeaguesController < ApplicationController
+
+    def create
+        league = League.create(league_params)
+        render json: league
+    end
+
+    private
+
+    def league_params
+        params.require(:league).permit(:user_id, :name)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   post '/login', to: 'users#login'
+  resource :leagues, only: [:create]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
